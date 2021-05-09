0d83c1830b476b7418508ce8b5f77b75ffe9b194
Mohan Gangadhar Gudey <mohan.gangadhar@gmail.com>
2021-05-09 20:12:00 +0530

    Added loader

---
+++ src/OrderCard.js
@@ -0,0 +1,89 @@
+import React, { Component } from "react";
+import { NotificationManager } from 'react-notifications';
+
+class OrderCard extends Component {
+    constructor(props) {
+        super(props);
+        this.updateReferral = this.updateReferral.bind(this);
+        this.saveOrder = this.saveOrder.bind(this);
+        this.state = {
+            orderInfo: this.props.orderInfo
+        }
+    }
+    saveOrder() {
+        console.log(this.state.orderInfo);
+        fetch(this.state.url, {
+            method: 'PUT', // or 'PUT'
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': 'true'
+            },
+            body: JSON.stringify(this.state.orderInfo),
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+                NotificationManager.success('You changes have been updated!', 'Successful!', 1000);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+                NotificationManager.error('Error while making your changes, contact support!', 'Error!');
+            });
+    }
+    updateReferral(event) {
+        const { orderInfo } = { ...this.state };
+        const currentState = orderInfo;
+        const { name, value } = event.target;
+        currentState[name] = value;
+        currentState["type"] = "order";
+        this.setState({
+            orderInfo: currentState
+        });
+    }
+    render() {
+        return (
+            <div className="card">
+                <div className="card-body">
+                    <div className="form-group row">
+                        <label className="col-sm-2 col-form-label col-form-label-md">Order No :</label>
+                        <label className="col-sm-4 col-form-label col-form-label-md">{this.state.orderInfo.orderId}</label>
+                        <label className="col-sm-2 col-form-label col-form-label-md">Name :</label>
+                        <label className="col-sm-4 col-form-label col-form-label-md">{this.state.orderInfo.custFirstName} {this.state.orderInfo.custLastName} </label>
+                    </div>
+                    <div className="form-group row">
+                        <label className="col-sm-2 col-form-label col-form-label-md">Email :</label>
+                        <label className="col-sm-4 col-form-label col-form-label-md">{this.state.orderInfo.email}</label>
+                        <label className="col-sm-2 col-form-label col-form-label-md">Phone :</label>
+                        <label className="col-sm-4 col-form-label col-form-label-md">{this.state.orderInfo.phone} </label>
+                    </div>
+                    <div className="form-group row">
+                        <label className="col-sm-2 col-form-label col-form-label-md">Total :</label>
+                        <label className="col-sm-4 col-form-label col-form-label-md">
+                            <button className="btn btn-info btn-disabled">{this.state.orderInfo.total}</button>
+                        </label>
+                        <label className="col-sm-2 col-form-label col-form-label-md">Referral  :</label>
+
+                        {this.state.orderInfo.referral.length < 0 ?
+                            <label className="col-sm-4 col-form-label col-form-label-md"> {this.state.orderInfo.referral} </label> :
+                            (<div className="col-sm-3 dropdown">
+                                <select name="referral" className="custom-select" value={this.state.orderInfo.referral} onChange={this.updateReferral} >
+                                    <option>Select..</option> {this.props.refval.map((val) => (<option value={val}>{val}</option>))};
+                          </select>
+                            </div>)}
+                    </div>
+                    <div className="form-group row">
+                        <label className="col-sm-2 col-form-label col-form-label-md">Comments :</label>
+                        <input type="text" className="col-sm-9 form-control"></input>
+                    </div>
+                    <div className="form-group row">
+                        <button type="button" onClick={this.saveOrder} className="col-sm-2 col-form-label btn btn-primary btn-md">Save</button>
+                    </div>
+                </div>
+            </div>
+
+        )
+    }
+}
+
+export default OrderCard;
--- src/Summary.js
+++ src/Summary.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 import ProductCard from './ProductCard.js';
-import { NotificationManager } from 'react-notifications';
+import OrderCard from './OrderCard.js';
+import Spinner from './spinner.js';
+
 
 class Summary extends Component {
   constructor(props) {
     super(props);
     this.searchOrder = this.searchOrder.bind(this);
-    this.updateReferral = this.updateReferral.bind(this);
-    this.saveOrder = this.saveOrder.bind(this);
     this.state = {
-      todos: [],
+      loading: false,
       orderVal: [],
       productVal: [],
       inputValue: '',
@@ -33,50 +33,39 @@ class Summary extends Component {
       inputValue: event.target.value
     });
   }
-  updateReferral(event) {
-    const { orderInfo } = { ...this.state };
-    const currentState = orderInfo;
-    const { name, value } = event.target;
-    currentState[name] = value;
-    currentState["type"] = "order";
-    this.setState({
-      orderInfo: currentState
-    });
-  }
   searchOrder() {
+    this.setState({ loading: true });
     fetch(this.state.url + '/' + this.state.inputValue)
       .then(res => res.json())
       .then((data) => {
         this.setState({
           orderVal: JSON.parse(data.order_result).Items,
           orderInfo: JSON.parse(data.order_result).Items[0],
-          productVal: JSON.parse(data.product_result).Items
+          productVal: JSON.parse(data.product_result).Items,
+          loading: false
         });
       })
       .catch(console.log)
   }
-  saveOrder() {
-    console.log(this.state.orderInfo);
-    fetch(this.state.url, {
-      method: 'PUT', // or 'PUT'
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': 'true'
-      },
-      body: JSON.stringify(this.state.orderInfo),
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log('Success:', data);
-        NotificationManager.success('You changes have been updated!', 'Successful!', 1000);
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-        NotificationManager.error('Error while making your changes, contact support!', 'Error!');
-      });
-  }
+
   render() {
+    const orderVal = this.state.orderVal;
+    const loading = this.state.loading;
+    let orders;
+    if (orderVal === null || loading) {
+      orders = <Spinner />;
+    } else {
+      if (orderVal) {
+        orders = orderVal.map((order, index) => (
+          <OrderCard orderInfo={order} index={index} key={index}
+            refval={this.state.refval}
+            url={this.state.url} />
+        ))
+      } else {
+        orders = <h3 className="text-center">There is no order recored!</h3>;
+      }
+    }
+
     return (
       <div className="container">
         <div className="col-xs-12">
@@ -92,61 +81,7 @@ class Summary extends Component {
               </div>
             </div>
           </div>
-          {this.state.orderVal.map((order) => (
-            <div className="card">
-              <div className="card-body">
-                <div className="form-group row">
-                  <label className="col-sm-2 col-form-label col-form-label-md">Order No :</label>
-                  <label className="col-sm-4 col-form-label col-form-label-md">{order.orderId}</label>
-                  <label className="col-sm-2 col-form-label col-form-label-md">Name :</label>
-                  <label className="col-sm-4 col-form-label col-form-label-md">{order.custFirstName} {order.custLastName} </label>
-                </div>
-                <div className="form-group row">
-                  <label className="col-sm-2 col-form-label col-form-label-md">Email :</label>
-                  <label className="col-sm-4 col-form-label col-form-label-md">{order.email}</label>
-                  <label className="col-sm-2 col-form-label col-form-label-md">Phone :</label>
-                  <label className="col-sm-4 col-form-label col-form-label-md">{order.phone} </label>
-                </div>
-                <div className="form-group row">
-                  <label className="col-sm-2 col-form-label col-form-label-md">Total :</label>
-                  <label className="col-sm-4 col-form-label col-form-label-md">
-                    <button className="btn btn-info btn-disabled">{order.total}</button>
-                  </label>
-                  <label className="col-sm-2 col-form-label col-form-label-md">Referral  :</label>
-
-                  {order.referral.length < 0 ?
-                    <label className="col-sm-4 col-form-label col-form-label-md"> {order.referral} </label> :
-                    (<div className="col-sm-3 dropdown">
-                      <select name="referral" className="custom-select" value={this.state.orderInfo.referral} onChange={this.updateReferral} >
-                        <option>Select..</option> {this.state.refval.map((val) => (<option value={val}>{val}</option>))};
-                      </select>
-                    </div>)}
-                </div>
-                <div className="form-group row">
-                  <label className="col-sm-2 col-form-label col-form-label-md">Comments :</label>
-                  <input type="text" className="col-sm-9 form-control"></input>
-                </div>
-                <div className="form-group row">
-                  <button type="button" onClick={this.saveOrder} className="col-sm-2 col-form-label btn btn-primary btn-md">Save</button>
-                </div>
-              </div>
-            </div>
-          ))}
-          {/* "orderId": 5848,
- "id": "ef7b899a-1377-4ab1-bacc-f22db6f68481",
- "tracking_poc": "Vishnu",
- "feedback_poc": "Vishnu",
- "delivered_by": "Vishnu",
- "product_status": "Delivered",
- "total": "1150",
- "createdAt": 5,
- "ordering_poc": "Vishnu",
- "quality_by": "Mohan",
- "payment_poc": "Mohan",
- "name": "Alphonso Devgad - 1 Dozen",
- "packed_by": "queen",
- "updatedAt": 5,
- "productId": 4024 */}
+          {orders}
           {this.state.productVal.map((product, index) => (
             <ProductCard product={product} index={index}
               key={index}
